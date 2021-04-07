f285e00e258976662a310f4b93e63fe7b6dbea88
Frankxhy <54780802+Frankxhy@users.noreply.github.com>
2021-04-07 15:09:23 +0800

    1

--- Kindergarten Literacy/Vowel Letter/VowelLetterViewController.swift
+++ Kindergarten Literacy/Vowel Letter/VowelLetterViewController.swift
@@ -9,11 +9,44 @@ import Foundation
 import UIKit
 
 class VowelLetterViewController: UIViewController {
+    
+    var array = ["a", "a ", "e", "i", "i ", "o", "u"]
+    
+    let i0 = UIImage(named: "level_button_0_star")
+    let i1 = UIImage(named: "level_button_1_star")
+    let i2 = UIImage(named: "level_button_2_star")
+    let i3 = UIImage(named: "level_button_3_star")
+    let i4 = UIImage(named: "level_button_4_star")
+    let i5 = UIImage(named: "level_button_5_star")
+    var starsImages: [UIImage?] { return [self.i0, self.i1, self.i2, self.i3, self.i4, self.i5] }
+
+    // collection of all buttons in current page
+    @IBOutlet var levelButtons: [UIButton]!
+    
+    func initButtonBackground() {
+        // get savefile from userdefaults
+        let letterStarCount = UserDefaults.standard.dictionary(forKey: "letterStarCount")
+        for button in levelButtons {
+            let currentLetter = button.titleLabel!.text!
+            let currentStarCount = letterStarCount![currentLetter] as! Int
+            button.setBackgroundImage(starsImages[currentStarCount], for: .normal)
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        for button in levelButtons {
+            button.setBackgroundImage(UIImage(named: "level_button_0_star"), for: .normal)
+            // button.layer.cornerRadius = 20
+            button.contentHorizontalAlignment = .left
+            button.contentVerticalAlignment = .top
+            button.titleEdgeInsets = UIEdgeInsets(top: 20, left: 40, bottom: 0, right: 0)
+        }
+        
+        
     }
     
     @IBAction func RealVowelLetter(_ sender: Any) {
@@ -33,8 +66,11 @@ class VowelLetterViewController: UIViewController {
 
 
     @IBAction func quiz(_ sender: Any) {
-        let vc = storyboard?.instantiateViewController(identifier: "vowel_quiz_vc") as! BeginningSoundsQuizViewController
+        let vc = storyboard?.instantiateViewController(identifier: "vowel_quiz_vc") as! VowelQuizViewController
+        vc.passedInLetter = array.randomElement()
+        vc.passedInArray = array
         present(vc, animated: true)
+
     }
     
     @IBAction func coin(_ sender: Any) {
